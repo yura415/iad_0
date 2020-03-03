96cb8d71512da3f5d1e09548532f9a5a4d29ce2e
yura415 <yura@api.haus>
2020-03-03 22:56:37 +0300

    upload

---
+++ precache-manifest.9585d4df08b2c44d70e2ba7d8e07d303.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ebc564266f777a0019f7471dc7682dc",
+    "url": "./assets/images/letters/character_rus_3.json"
+  },
+  {
+    "revision": "426f3f40ca7c76dd1141",
+    "url": "./js/bundle.js"
+  },
+  {
+    "revision": "31e0a0cfbb42ad4973a7",
+    "url": "./js/vendor.bundle.js"
+  },
+  {
+    "revision": "b45318f18b86db69eb8d2561e9219368",
+    "url": "./assets/images/letters/character_rus_2.json"
+  },
+  {
+    "revision": "4b191881072b32741315395b61944d4f",
+    "url": "./assets/images/atlas1.png"
+  },
+  {
+    "revision": "b0b4e8487f00afa35827889c15ba7048",
+    "url": "./assets/images/letters/character_rus_1.atlas"
+  },
+  {
+    "revision": "f1cd4872e39551eefed65d1e0ce4fe12",
+    "url": "./assets/images/letters/character_rus_19.atlas"
+  },
+  {
+    "revision": "e3df9f6f4d385b28d618fd7233af517f",
+    "url": "./assets/images/letters/character_rus_2.atlas"
+  },
+  {
+    "revision": "6eb49d3240d1b6dd4eddc151a37cee83",
+    "url": "./assets/images/letters/character_rus_3.atlas"
+  },
+  {
+    "revision": "dfb749fb41edc5c53626b50ec9d48776",
+    "url": "./assets/images/loader-bg.png"
+  },
+  {
+    "revision": "b8aa464daed86a9610993229c74e5c78",
+    "url": "./assets/sounds/sprite.json"
+  },
+  {
+    "revision": "7ddcbaac0638626d353d87b280875ae8",
+    "url": "./assets/sounds/sprite.mp3"
+  },
+  {
+    "revision": "dfd050ec6af9bb3159079d36e90f56d9",
+    "url": "./assets/sounds/sprite.ac3"
+  },
+  {
+    "revision": "78ea0555ef7648bf378481a5d778d344",
+    "url": "./assets/images/loader-bar.png"
+  },
+  {
+    "revision": "3ece71d039d6f504f656c184d2c8510e",
+    "url": "./assets/images/images/cake.png"
+  },
+  {
+    "revision": "500204aa42b710c61b7253578c5424f9",
+    "url": "./assets/images/letters/character_rus_1.json"
+  },
+  {
+    "revision": "b12801c45683578034e1aa9882207119",
+    "url": "./assets/images/letters/character_rus_19.json"
+  },
+  {
+    "revision": "ba77c92b32b078318136951bfb333743",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7247e1850ffae85b7ba4dc4be02ead2d",
+    "url": "./assets/images/images/vinograd.png"
+  },
+  {
+    "revision": "5ea59c2c72a781323fb4207a78719fea",
+    "url": "./assets/sounds/sprite.ogg"
+  },
+  {
+    "revision": "94a071adfbe54ff1a7273df4e786530d",
+    "url": "./assets/sounds/sprite.m4a"
+  },
+  {
+    "revision": "07fd57927d901bbc02d1edfc68abe8ab",
+    "url": "./assets/images/images/ball.png"
+  },
+  {
+    "revision": "72074026a5efba56d0edc53f6a52d125",
+    "url": "./assets/images/images/baraban.png"
+  },
+  {
+    "revision": "c55b23e6a4aec981a48420745cf0f08d",
+    "url": "./assets/images/letters/character_rus_19.png"
+  },
+  {
+    "revision": "8f8016a29255560acfd8b682835b0f2e",
+    "url": "./assets/images/letters/character_rus_1.png"
+  },
+  {
+    "revision": "789f9bdd02a8c277a558e182b19a4adb",
+    "url": "./assets/images/letters/character_rus_3.png"
+  },
+  {
+    "revision": "b6e48c280e99b0c8efc284e3d9e3fa6c",
+    "url": "./assets/images/images/apple.png"
+  },
+  {
+    "revision": "9d9dae96068a29a3ae2ce59af697cafa",
+    "url": "./assets/images/letters/character_rus_2.png"
+  },
+  {
+    "revision": "4cdafa9a2b5fcbc27ab7a79050fcf9e4",
+    "url": "./assets/images/bg.png"
+  },
+  {
+    "revision": "4176ee31e460a840975259f5d2d4feb5",
+    "url": "./assets/images/images/arbuz.png"
+  },
+  {
+    "revision": "b9b1354f7e337a13b284c02f35364542",
+    "url": "./assets/images/atlas1.json"
+  },
+  {
+    "revision": "1aa5f7685a710af9a2f2f4d8f3b71db4",
+    "url": "./assets/images/.DS_Store"
+  },
+  {
+    "revision": "431675caa41d65dec17439b87e212e3b",
+    "url": "./assets/.DS_Store"
+  },
+  {
+    "revision": "be531d82452e8a8e5b0ce102bda275ff",
+    "url": "././manifest.json"
+  }
+];
